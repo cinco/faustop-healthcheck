33321fe812269712e34d558d92157dcdda62fd4f
Thiago Euzebio <thiagoeuzebio@gmail.com>
2023-05-26 02:40:49 -0300

    🐛 fix(composer.json): add laravel dependency 🎉 feat(HealthCheckController.php): add error response when database is down

--- src/Http/Controllers/HealthCheckController.php
+++ src/Http/Controllers/HealthCheckController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
-use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -13,34 +12,23 @@ class HealthCheckController extends Controller
 {
     public function healthCheck()
     {
-        try{
-
+        try {
             $redis = Redis::connection();
             $DB = DB::connection();
 
-            if($redis->ping()){
-                $redisPing = 'Running at '.Carbon::now();
-            }else{
-                $redisPing = 'down';
-            }
-
-            if($DB->getPDO()){
-                $DBPing = 'Running at '.Carbon::now();
-            }else{
-                $DBPing = 'down';
-            }
+            $redisPing = $redis->ping() ? 'Running at ' . Carbon::now() : 'down';
+            $DBPing = $DB->getPdo() ? 'Running at ' . Carbon::now() : 'down';
 
             return [
                 'redis' => $redisPing,
                 'DB' => $DBPing
             ];
-
-        } catch(\Throwable $th) {
-            Log::error($_SERVER['HOSTNAME'] . ' - ['.$_SERVER["APP_NAME"].'] - Healthcheck: database is down');
+        } catch (\Throwable $th) {
+            Log::error($_SERVER['HOSTNAME'] . ' - [' . $_SERVER["APP_NAME"] . '] - Healthcheck: database is down');
             Log::error($th->getMessage());
+
+            $message_error = '<img src="https://j.gifs.com/m8LGL3.gif"></img>';
+            return response('Database is <b>down</b> <br><img src="https://j.gifs.com/m8LGL3.gif"></img><br> Database is <b>down</b><br> msg from: <b>' . $_SERVER['HOSTNAME'] . '</b>', 500);
         }
-        $message_error = '<img src="https://j.gifs.com/m8LGL3.gif"></img>';
-        // return view('welcome', ['message_error' => $message_error ]);
-        return response('Database is <b>down</b> <br><img src="https://j.gifs.com/m8LGL3.gif"></img><br> Database is <b>down</b><br> msg from: <b>' . $_SERVER['HOSTNAME'] .'</b>', 500);
     }
 }
