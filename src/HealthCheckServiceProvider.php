80290069e5b808db3e9a5628244e428028666eeb
Thiago Euzebio <thiagoeuzebio@gmail.com>
2023-05-26 10:43:05 -0300

    ✨ feat(HealthCheckServiceProvider.php): add HealthCheckServiceProvider class with loadRoutesFrom method

---
+++ src/HealthCheckServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cinco\FaustopHealthcheck;
+
+use Illuminate\Support\ServiceProvider;
+
+class HealthCheckServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+}
